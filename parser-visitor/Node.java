585e1fd17f7a820c545630b375f650b7a3e9b739
nineil-weber <109441984+nineil-weber@users.noreply.github.com>
2023-09-14 14:38:13 -0500

    Add files via upload

---
+++ parser-visitor/Node.java
@@ -0,0 +1,94 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.*;
+
+/**
+ *
+ * @author nineil
+ */
+abstract class Node {
+        Node parent;
+        int line=-1;
+        int col=-1;
+        ArrayList<Node> children;
+        public void setPosition(int l, int c)
+        {
+            line=l;
+            col=c;
+        }
+        Node() { children = new ArrayList<Node>(); }
+        public abstract int accept(Visitor v);
+}
+
+class IntNode extends Node {
+        Integer myInt;
+
+        public IntNode(Integer i, int l, int c)
+        {
+            myInt = i;
+            setPosition(l, c);
+        }
+
+        public int accept(Visitor v)
+        {
+            return v.visit(this);
+        }
+}
+
+class AddNode extends Node {
+        public AddNode(Node l, Node r, int pl, int pc) {
+            children.add(l);
+            children.add(r);
+            setPosition(pl, pc);
+        }
+
+        public int accept(Visitor v)
+        {
+            return v.visit(this);
+        }
+}
+
+class MinusNode extends Node {
+        public MinusNode(Node l, Node r, int pl, int pc) {
+            children.add(l);
+            children.add(r);
+            setPosition(pl, pc);
+        }
+
+        public int accept(Visitor v)
+        {
+            return v.visit(this);
+        }
+}
+
+class TimesNode extends Node {
+
+        public TimesNode(Node l, Node r, int pl, int pc) {
+            children.add(l);
+            children.add(r);
+            setPosition(pl, pc);
+        }
+
+        public int accept(Visitor v)
+        {
+            return v.visit(this);
+        }
+}
+
+class DivNode extends Node {
+
+        public DivNode(Node l, Node r, int pl, int pc) {
+            children.add(l);
+            children.add(r);
+            setPosition(pl, pc);
+        }
+
+        public int accept(Visitor v)
+        {
+            return v.visit(this);
+        }
+}
+
